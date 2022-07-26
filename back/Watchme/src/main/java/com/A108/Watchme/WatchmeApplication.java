a28253abe597b38c7cee46c20ad569fdd09ad20c
Denny-Ko <donghar@naver.com>
2022-07-27 00:59:17 +0900

    DEL-unnecessary imports

--- back/Watchme/src/main/java/com/A108/Watchme/WatchmeApplication.java
+++ back/Watchme/src/main/java/com/A108/Watchme/WatchmeApplication.java
@@ -1,9 +1,5 @@
 package com.A108.Watchme;
 
-import com.A108.Watchme.Repository.MemberRepository;
-import com.A108.Watchme.VO.Member;
-import com.A108.Watchme.VO.Status;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

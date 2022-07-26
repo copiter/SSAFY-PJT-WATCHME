03cdb65cf422cb8141eb872162c8e4ee6a447352
copiter <glowdonor@gmail.com>
2022-07-26 10:41:53 +0900

    FEAT-RoomDetail.js 기초 설계
    RoomDetail.js 뼈대 설계

--- front-run/src/components/Room/RoomDetail.js
+++ front-run/src/components/Room/RoomDetail.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+import out from "../../img/Icons/out.png";
+import headset from "../../img/Icons/headset.png";
+import headset_mute from "../../img/Icons/headset_mute.png";
+import mic from "../../img/Icons/mic.png";
+import mic_mute from "../../img/Icons/mic_mute.png";
+import setting from "../../img/Icons/setting.png";
+import share_video from "../../img/Icons/share_video.png";
+
+import "./RoomDetail.css";
 
 function RoomDetail() {
+  const [isMicOn, setIsMicOn] = useState(false);
+
   return (
     <>
-      <h1>실제 방 페이지입니다..</h1>
-      <p>현재 구현 예정입니다.</p>
+      <div className="room">
+        <div className="nav">
+          <button type="button">방장설정</button>
+        </div>
+        <div className="body">
+          <div className="webrtc">webrtc 페이지</div>
+          <div className="side">side bar</div>
+        </div>
+        <div className="footer">
+          <img src={share_video} alt="out" width={30} />
+          <img src={mic} alt="out" width={30} />
+
+          <img src={mic_mute} alt="out" width={30} />
+          <img src={headset} alt="out" width={30} />
+          <img src={headset_mute} alt="out" width={30} />
+          <img src={setting} alt="out" width={30} />
+          <img src={out} alt="out" width={30} />
+        </div>
+      </div>
     </>
   );
 }

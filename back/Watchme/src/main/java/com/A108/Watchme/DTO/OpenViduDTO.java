cd286aa7269bb3d5badb323c6bb844615ef3d301
Fe26Atom <pcm0720@gmail.com>
2022-07-28 22:38:53 +0900

    TEST-OpenVidu테스트
    TEST=OpenVidu테스트 (OpenVidu_Tutorial)

---
+++ back/Watchme/src/main/java/com/A108/Watchme/DTO/OpenViduDTO.java
@@ -0,0 +1,10 @@
+package com.A108.Watchme.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OpenViduDTO {
+    private String secret;
+    private String openviduUrl;
+}

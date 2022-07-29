6a1f0ff78ada4c1562fef6612664275590be788c
Fe26Atom <pcm0720@gmail.com>
2022-07-29 14:56:26 +0900

    FEAT-WebRtc기능 구현(방 참가, 방 나가기)
    FEAT-WebRtc기능 구현(방 참가, 방 나가기)

--- back/Watchme/src/main/java/com/A108/Watchme/DTO/RoomLeaveDTO.java
+++ back/Watchme/src/main/java/com/A108/Watchme/DTO/RoomLeaveDTO.java
@@ -1,4 +1,11 @@
 package com.A108.Watchme.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class RoomLeaveDTO {
+    private Long memberId;
+    private Long roomId;
+    private String roomToken;
 }

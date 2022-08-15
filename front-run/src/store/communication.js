02280a00519f1ee5fb486e44c57d7d99c58df843
김영훈 <redwhich@gmail.com>
2022-08-15 23:06:16 +0900

    REFACTOR-수정사항올립니다
    수정사항 올립니다.

--- front-run/src/store/communication.js
+++ front-run/src/store/communication.js
@@ -1,4 +1,3 @@
 import React from "react";
-
-// export const FetchUrl = React.createContext("https://watchme1.shop/api");
+//export const FetchUrl = React.createContext("https://watchme1.shop/api");
 export const FetchUrl = React.createContext("http://localhost:81");

e2d69e743aa130eb68b8617631f14fb6b38bab1f
Denny-Ko <donghar@naver.com>
2022-08-18 02:12:35 +0900

    ADD-API-check validation

--- back/Watchme/src/main/java/com/A108/Watchme/DTO/Room/PostRoomReqDTO.java
+++ back/Watchme/src/main/java/com/A108/Watchme/DTO/Room/PostRoomReqDTO.java
@@ -3,10 +3,7 @@ package com.A108.Watchme.DTO.Room;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 
 @Getter @Setter
 public class PostRoomReqDTO {
@@ -20,6 +17,7 @@ public class PostRoomReqDTO {
     private String categoryName;
     @NotNull(message = "인원을 입력하세요.")
     @Positive(message = "인원은 0보다 큰 값이 들어가야 합니다.")
+    @Max(value = 25)
     private Integer num;
     private String endTime;
 }
